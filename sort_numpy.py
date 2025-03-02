f7360b60f70b25d9ced081665b4d313b7bbccbc1
NguyenQuocPhu <maxclass6i@gmail.com>
2025-03-02 15:10:23 +0700

    Update sort_numpy.py

--- sort_numpy.py
+++ sort_numpy.py
@@ -5,9 +5,12 @@ for i in range(1,10):
     filename = "test_" + str(i) + ".inp"
     with open(filename, "r", encoding = "utf-8") as file:
         arr = file.read()
+    # Lấy dữ liệu trong file
     a = np.fromstring(arr, dtype = int, sep = " ")
+    # Chuyển đổi dữ liệu từ string sang int
     st = time.time()
     sorted_arr = np.sort(a)
     en = time.time()
     runningtime = (en - st) * 1000
+    # Tính thời gian chạy của mỗi file
     print(f"Runtime: {runningtime:.3f} ms")
