2be2e26231e6693c7310e1dad8f39c85e4f8d39c
NguyenQuocPhu <maxclass6i@gmail.com>
2025-03-02 15:07:31 +0700

    Update quickSort_c.cpp

--- quickSort_c.cpp
+++ quickSort_c.cpp
@@ -37,11 +37,11 @@ int SIZE = 1e6;
 int main() {
     srand(time(0));
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    # Lấy liên tiếp 10 test để kiểm tra
+    // Lấy liên tiếp 10 test để kiểm tra
     for(int test = 1 ; test <= 10 ; test++){
         string filename = "test_" + to_string(test) + ".txt";
         freopen(filename.c_str(), "r", stdin);
-        # Lấy dữ liệu từ trong file
+        // Lấy dữ liệu từ trong file
         auto start = std::chrono::high_resolution_clock::now();
         for (int i = 1 ; i <= SIZE; i++){
             int x; cin>>x;
@@ -51,7 +51,7 @@ int main() {
         auto end = std::chrono::high_resolution_clock::now();
         a.clear();
         std::chrono::duration<double, std::milli> elapsed = end - start;
-        # Tính thời gian chạy của mỗi file 
+        // Tính thời gian chạy của mỗi file 
         std::cout << "Runtime "<<test<<": "<< elapsed.count() << " ms" << std::endl;
     }
     return 0;
